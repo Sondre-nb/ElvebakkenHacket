3261301469461454651e6c93caf4c8cbc9d82b64
Sondre <sondre.njastad.bjorn@gmail.com>
2025-02-03 11:04:45 +0100

    La til URL og tester om jeg kan få startssiden til å fungere

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function check(targetValue) {
     return inputValue === targetValue;
 }
 
-function attemptAnswer(level, levels) {
+function attemptAnswer(level) { //levels var et argument her før, vet ikke om du hadde none stor plan, Trym, men tror det skal fungere uten
     const revEncodedTargetValue = levels[level][0];
     const revEncodedNextPageURL = levels[level][1];
     const targetValue = njdasjAKSJ276tdSGDU(idjoisaNU45asdSbds72GDUSA(revEncodedTargetValue));
@@ -24,6 +24,14 @@ function attemptAnswer(level, levels) {
     }
 }
 
-levels = [ // nextPageURL (reversed), targetValue (reversed)
-    ["=02bj5yajl2c","=03MyAiOvl3e"]
+let levels = [ // nextPageURL (reversed), targetValue (reversed)
+    ["vM3a1pUZyRmbphUYBJ3bGFmc0N3aFV2bORWZtJTZkl2cvQXZrNWYI5WZrtWYiVmdsV0LvlmLiVHa0l2ZuImbtUmck52bz9yL6MHc0RHa","=IXZkx2boV2YhxGc"], //targetValue er "placeholder" bare kjørt gjennom FJERNFØRLAUNCH.js
+    ["==wLndWZsxWaUlEdl5mbBV2bORWZtNTZkl2cvQXZrNWYI5WZrtWYiVmdsV0LvlmLiVHa0l2ZuImbtUmck52bz9yL6MHc0RHa","placeholder"],
+    ["==wLuVGTSVVZ0RXZqdUZu5WdLVHRnlGbltmcpZVdEVGZk9mc0RTZkl2cvQXZrNWYI5WZrtWYiVmdsV0LvlmLiVHa0l2ZuImbtUmck52bz9yL6MHc0RHa", "placeholder"],
+    ["vQXZEVHRyVmblpGdy9mRl5mbuVGRlRHdlp2RhFEdyFGbLJXYIVHRzlmdoVTZkl2cvQXZrNWYI5WZrtWYiVmdsV0LvlmLiVHa0l2ZuImbtUmck52bz9yL6MHc0RHa", "placeholder"],
+    ["==wLlR3cpNVZExWaUV2bOFWYQVmbulmRlJXYCdWZKFWYNFWYuZTZkl2cvQXZrNWYI5WZrtWYiVmdsV0LvlmLiVHa0l2ZuImbtUmck52bz9yL6MHc0RHa", "placeholder"],
+    ["==wLuVmanlkclRWaTFWYG5WZv5Ed0F2c0J3bmdTZkl2cvQXZrNWYI5WZrtWYiVmdsV0LvlmLiVHa0l2ZuImbtUmck52bz9yL6MHc0RHa", "placeholder"],
+    ["=8iclVGZJJ3bG12bUFWYHFWQnlGbltmcpZlcl5mb5dWZCdWZqhTZkl2cvQXZrNWYI5WZrtWYiVmdsV0LvlmLiVHa0l2ZuImbtUmck52bz9yL6MHc0RHa", "placeholder"],
+    ["=8iclxkUVFWYQVmbulmRhFEZl10ZpRmclZEdyFmbTdWZKJXRnlGblRmbllTZkl2cvQXZrNWYI5WZrtWYiVmdsV0LvlmLiVHa0l2ZuImbtUmck52bz9yL6MHc0RHa", "placeholder"],
+    ["=8iblxkUVlUYyR3crVUZv5EZl1WZkl2cyVmbulmdvQXZrNWYI5WZrtWYiVmdsV0LvlmLiVHa0l2ZuImbtUmck52bz9yL6MHc0RHa", "placeholder"]
 ]
