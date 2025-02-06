6a3ea15554d2fde9fabd5c5945f39e7537ea7ddc
Trym Bringsrud Fekjær <trym.mcduck@gmail.com>
2025-02-06 08:27:11 +0100

    Fiksfiks

--- FJERNFØRLAUNCH.js
+++ FJERNFØRLAUNCH.js
@@ -14,5 +14,5 @@ function decodeScrambledBase64(str) {
     return atob(idjoisaNU45asdSbds72GDUSA(str));
 }
 
-console.log(createScrambledBase64("/ElvebakkenHacket/side3medNoeAnnetITillegg/"));
+console.log(createScrambledBase64("0 :  {fornavn: 'Hans', etternavn: 'Mikkelsen', fodselsaar: 1989, minutter: 40} 1 :  {fornavn: 'Mikkel', etternavn: 'Hansen', fodselsaar: 2005, minutter: 9} 2 :  {fornavn: 'Eikik', etternavn: 'Lie', fodselsaar: 2006, minutter: 1}"));
 console.log(decodeScrambledBase64("=03MyAiOvl3e"));
